a28b84235f93bd7efd2ff8cecab8815635d6f548
Siddharth-Agg-arwal <aggarwal.siddharth2003@outlook.com>
2025-02-04 03:09:24 +0530

    init

---
+++ app/landing-page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "./css/landingpage.module.css";
+
+export default function LandingPage() {
+  return (
+    <div>
+        landing page
+    </div>
+  );
+}
