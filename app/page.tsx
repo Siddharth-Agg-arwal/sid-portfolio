3967f9c296da58ecd291e63a7c19f8d95e59b599
Siddharth-Agg-arwal <aggarwal.siddharth2003@outlook.com>
2025-02-14 19:03:32 +0530

    work init

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import MusicPage from "./set-music";
 import AboutMe from "./about-me";
 import Navbar from "@/components/aboutme/navbar";
 import Resume from "./resume";
+import Works from "./works";
 
 export default function Home() {
   return (
@@ -18,6 +19,7 @@ export default function Home() {
       <section id="about-me"><AboutMe /></section>
       <hr></hr>
       <section id="resume"><Resume /></section>
+      <section id="works"><Works /></section>
       <hr></hr>
     </div>
   );
---
+++ app/works.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image"
+
+export default function Works(){
+    return (
+        <div>
+            <h1>Works</h1>
+            <Image src="/works/cilp.png" alt="Works" width={900} height={600} />
+        </div>
+    )
+}
