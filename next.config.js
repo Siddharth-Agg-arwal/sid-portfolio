e7ed0379777e0bb11285d4e3e33c72f3e1f4d070
Siddharth-Agg-arwal <aggarwal.siddharth2003@outlook.com>
2025-03-21 22:12:10 +0530

    prod issues fixed

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
 
-const nextConfig: NextConfig = {
+const nextConfig = {
+  reactStrictMode: true,
   images: {
     remotePatterns: [
       {
@@ -8,88 +9,63 @@ const nextConfig: NextConfig = {
         hostname: 'res.cloudinary.com',
         port: '',
         pathname: '/danxppoxy/image/upload/v1742572785/Sidcom/coldplay_bhjgji.png',
-        search: '',
       },
       {
         protocol: 'https',
         hostname: 'res.cloudinary.com',
         port: '',
         pathname: '/danxppoxy/image/upload/v1742572789/Sidcom/kendrick_nfc4k6.png',
-        search: '',
       },
-
       {
         protocol: 'https',
         hostname: 'res.cloudinary.com',
         port: '',
         pathname: '/danxppoxy/image/upload/v1742572787/Sidcom/malone_cowbey.png',
-        search: '',
       },
-
       {
         protocol: 'https',
         hostname: 'res.cloudinary.com',
         port: '',
         pathname: '/danxppoxy/image/upload/v1742572788/Sidcom/21pilots_uo3bhm.png',
-        search: '',
       },
-
       {
         protocol: 'https',
         hostname: 'res.cloudinary.com',
         port: '',
         pathname: '/danxppoxy/image/upload/v1742572785/Sidcom/travis_qvmmti.png',
-        search: '',
       },
-
       {
         protocol: 'https',
         hostname: 'res.cloudinary.com',
         port: '',
         pathname: '/danxppoxy/image/upload/v1742572789/Sidcom/joji_ziw7mi.png',
-        search: '',
       },
-
       {
         protocol: 'https',
         hostname: 'res.cloudinary.com',
         port: '',
         pathname: '/danxppoxy/image/upload/v1742572786/Sidcom/coldplay2_r2lffj.png',
-        search: '',
       },
-
       {
         protocol: 'https',
         hostname: 'res.cloudinary.com',
         port: '',
         pathname: '/danxppoxy/image/upload/v1742572788/Sidcom/metro_kflhod.png',
-        search: '',
       },
-
       {
         protocol: 'https',
         hostname: 'res.cloudinary.com',
         port: '',
         pathname: '/danxppoxy/image/upload/v1742572788/Sidcom/weeknd_dgg7be.png',
-        search: '',
       },
-
       {
         protocol: 'https',
         hostname: 'res.cloudinary.com',
         port: '',
         pathname: '/danxppoxy/image/upload/v1742572788/Sidcom/xxx_t7zojm.png',
-        search: '',
       },
-
     ],
   },
 };
 
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-})
-
-module.exports = withBundleAnalyzer(nextConfig)
-
-// export default nextConfig;
+module.exports = nextConfig;
